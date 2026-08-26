0b084230062b1fbeae42a03f41d760aba8f39eb3
mmadmehdi <golestn90@gmail.com>
2026-08-26 18:47:29 +0330

    Polish files

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-title Battery Alert 🔋
-powershell.exe -ExecutionPolicy Bypass -File "%~dp0src\battery_alert.ps1" %*
+title Battery Alert
+powershell.exe -ExecutionPolicy Bypass -File "%~dp0src\battery_alert.ps1"
 pause
